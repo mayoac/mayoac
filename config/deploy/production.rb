51de9cf82805fc001456616f36330767c452fc00
Mike Griffin <mgriffin@github.com>
2023-11-14 22:37:52 +0000

    Assign roles properly in capistrano config
    Found the solution at https://stackoverflow.com/a/35732693

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server "mayoac.pocleim.ie", roles: %[app db web]
+server "mayoac.pocleim.ie", roles: %w{app db web}
 
 # role-based syntax
 # ==================
